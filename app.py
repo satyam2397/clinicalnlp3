62da07bc07f9bdc192bdaf713a0625182baa0fee
satyam2397 <126483538+satyam2397@users.noreply.github.com>
2023-03-06 15:49:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ def lemmatize_col(sentence):
 #    return word_list
 #-----------------------------removing stop words------------------------------------------#
 sw_nltk = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-
 others_sw = ['please', 'get', 'problem','suggest', 'advise','help','take', 'also', 'sir', 'hello', 'hi' ,'hey', 'help']
 extended_sw = sw_nltk + others_sw
 
@@ -63,19 +62,9 @@ def remove_sw (text):
     new_text = " ".join([w for w in words])
     return new_text
 #-------------------------------------------------------------------------------------------#
-
-# geting and sending response to dialogflow
-@app.route('/webhook', methods=['POST'])
-@cross_origin()
-def webhook():
-
-    req = request.get_json(silent=True, force=True)
-    res = processRequest(req)
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
+vectorizar = pickle.load(open('tfidf/vectorizer.pkl', 'rb'))
+pks = [pickle.load(open("models_nlp/" + model, 'rb')) for model in os.listdir("models_nlp/")]
+#-------------------------------------------------------------------------------------------#
 # processing the request from dialogflow
 def processRequest(req):
 
@@ -92,20 +81,12 @@ def processRequest(req):
         queryText = remove_sw(queryText)
         queryText = rem_spcl_char(queryText)
         queryText = lemmatize_col(queryText)
-        
-        #-------------------------------------------------------------------------------------------#
-        vectorizar = pickle.load(open('tfidf/vectorizer.pkl', 'rb'))
         queryText_tfidf = vectorizar.transform([queryText])
-        #-------------------------------------------------------------------------------------------#
-    
-        my_path = "models_nlp/"
-        file_list = os.listdir(my_path)
-    
+
         res_final = pd.DataFrame()
-        for model in file_list:
+        for model in pks:
 #            try:
-            log_classifier = pickle.load(open(my_path + model, 'rb'))
-            prediction_prob = log_classifier.predict_proba(queryText_tfidf)
+            prediction_prob = model.predict_proba(queryText_tfidf)
             if prediction_prob[0][1] >= 0.1:
                 res = pd.DataFrame(prediction_prob, columns = ['prob0', 'prob1'])
                 res['disease'] = model[0:-4]
@@ -116,6 +97,20 @@ def processRequest(req):
         res_final = res_final.sort_values(['prob1'])
         fulfillmentText = ', '.join(res_final['disease'].tolist())
     return {"fulfillmentText": fulfillmentText}
+
+#-------------------------------------------------------------------------------------------#
+
+# geting and sending response to dialogflow
+@app.route('/webhook', methods=['POST'])
+@cross_origin()
+def webhook():
+
+    req = request.get_json(silent=True, force=True)
+    res = processRequest(req)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 #print("app_works")
 if __name__ == '__main__':
