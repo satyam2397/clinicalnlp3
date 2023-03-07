3d6008f44d4d697756d6cbd7bd4ee3bcf15af020
satyam2397 <126483538+satyam2397@users.noreply.github.com>
2023-03-07 11:49:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import pandas as pd
 import nltk
 from nltk.stem import WordNetLemmatizer 
 from nltk.corpus import wordnet
-#nltk.download('punkt')
+nltk.download('punkt')
 #nltk.download('wordnet')
 #nltk.download('averaged_perceptron_tagger')
 import warnings
